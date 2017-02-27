e4540c1185397485fd6060ec3873ec3836e23eec
shubhjagani <shubhjagani@users.noreply.github.com>
2017-02-27 15:44:11 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='skeleprint_ui',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/shubhjagani/skeleprint_ui',
+      author='Shubh Jagani',
+      author_email='shubh.jagani@gmail.com',
+      license='MIT',
+      packages=['skeleprint_ui'],
+      zip_safe=False)
